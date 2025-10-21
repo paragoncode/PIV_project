9da249d8009c3eb794253953b86a7c368cec3a5b
paragoncode <54541723+paragoncode@users.noreply.github.com>
2025-10-21 10:56:45 +0200

    player movement + jump + sprint

---
+++ PIVProject/Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpriteBillboard : MonoBehaviour
+{
+    [SerializeField] bool FreezeXZAxis = true;
+    
+    void Update()
+    {
+        if(FreezeXZAxis)
+        {
+            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        }
+        else
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
---
+++ PIVProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    private float speed = 2.5f;
+    public float moveSpeed;
+    public float sprintSpeed;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
+
+    private void StateHandler()
+    {
+        if (isGrounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            speed = sprintSpeed;
+        }
+
+        else if (isGrounded)
+        {
+            state = MovementState.walking;
+            speed = moveSpeed;
+        }
+
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateHandler();
+
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
