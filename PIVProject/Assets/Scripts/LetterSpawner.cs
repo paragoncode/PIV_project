44b7d897b693ef93edc8d340881e2fcc9aa4d73d
paragoncode <54541723+paragoncode@users.noreply.github.com>
2025-11-25 17:42:54 +0100

    some changes to letter spawn (not working)
    + make crucifix appear in hand

--- PIVProject/Assets/Scripts/LetterSpawner.cs
+++ PIVProject/Assets/Scripts/LetterSpawner.cs
@@ -28,13 +28,13 @@ public class LetterSpawner : MonoBehaviour
 
     private void SpawnLetter()
     {
-        Vector3 spawnPoint = new Vector3(Random.Range(-30, 30), 1f, Random.Range(-30, 30));
-        Instantiate(letter, spawnPoint, Quaternion.identity);
-        Debug.Log("Letter Spawned");
-        //if(!Physics.CheckSphere(spawnPoint, spawnCollisionCheckRadius))
-        //{
-            //Instantiate(letter, spawnPoint, Quaternion.identity);
-            //Debug.Log("Letter Spawned");
-        //}
+        Vector3 spawnPoint = new Vector3(Random.Range(-30, 0), 1f, Random.Range(-30, 0));
+        //Instantiate(letter, spawnPoint, Quaternion.identity);
+        //Debug.Log("Letter Spawned");
+        if(!Physics.CheckSphere(spawnPoint, spawnCollisionCheckRadius))
+        {
+            Instantiate(letter, spawnPoint, Quaternion.identity);
+            Debug.Log("Letter Spawned");
+        }
     }
 }
--- PIVProject/Assets/Scripts/PickUp.cs
+++ PIVProject/Assets/Scripts/PickUp.cs
@@ -7,19 +7,25 @@ public class PickUp : MonoBehaviour
 
     public GameObject pickUpText;
 
+    public GameObject objectOnPlayer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pickUpText.SetActive(false);
+        objectOnPlayer = GameObject.Find("First person player/Main Camera/Crucifix14");
+        objectOnPlayer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        objectOnPlayer = GameObject.Find("First person player/Main Camera/Crucifix14");
         if(Input.GetKeyDown(KeyCode.E) && checkPlayer)
         {
             Destroy(gameObject);
             pickUpText.SetActive(false);
+            objectOnPlayer.SetActive(true);
         }
     }
 
